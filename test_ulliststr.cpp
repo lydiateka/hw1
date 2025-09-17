c7fa26a295b835018f029ea125051ee8fbeacd45
Lydia Teka <teka@usc.edu>
2025-09-17 03:09:26 +0000

    all done with part 2

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -4,10 +4,25 @@
 #include <sstream>
 
 #include "ulliststr.h"
+using namespace std; 
 
 //Use this file to test your ulliststr implementation before running the test suite
 
 int main(int argc, char* argv[])
 {
+  ULListStr dat;
+  // dat.push_front("hello");
+  // dat.push_back("good");
+  // dat.push_back("bye");
+  // dat.push_front("New");
+  // dat.pop_front(); 
+
+  dat.pop_front(); 
+  cout << dat.size() << endl;
+  for (int i = 0; i < dat.size(); i++) {
+    cout << dat.get(i) << " "; 
+  }
+  cout << endl; 
+  cout << "Front: " << dat.front() << "  Back: " << dat.back() << endl; 
 
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,165 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_front(const std::string& val)
+{ 
+
+  //If the list is empty, create new node 
+  if (size_ == 0)
+  { 
+    Item* newItem = new ULListStr::Item; 
+    newItem->first = 9; 
+    newItem->last = 10;
+    newItem->val[9] = val; 
+    newItem->prev = nullptr; 
+    newItem->next = nullptr; 
+    head_ = newItem; 
+    tail_ = newItem; 
+  }
+
+  //If the current node has space at the front
+  else if (head_->first > 0) { 
+    --head_->first; 
+    head_->val[head_->first] = val;  
+  }
+
+  //If the current node is full, create a new head node
+  else if (head_->first == 0) {
+    Item* newItem = new ULListStr::Item; 
+    newItem->first = 9; 
+    newItem->last = 10;
+    newItem->next = head_;  
+    newItem->prev = nullptr; 
+    head_->prev = newItem; 
+    head_ = newItem; 
+    head_->val[9] = val; 
+  
+  }
+
+  size_++;
+  
+}
+
+void ULListStr::push_back(const std::string& val) 
+{
+  // If list is empty, create a new node
+ if (size_ == 0)
+  { 
+    Item* newItem = new ULListStr::Item; 
+    newItem->first = 0; 
+    newItem->last = 1;
+    newItem->val[0] = val; 
+    newItem->prev = nullptr; 
+    newItem->next = nullptr; 
+    head_ = newItem; 
+    tail_ = newItem; 
+  }
+
+  // If the current node has space at the end, add the value to the list
+  else if (tail_->last < 10) { 
+    tail_->val[tail_->last] = val;  
+    tail_->last++;  
+  }
+
+  //If the current node is full, create a new tail node 
+  else if (tail_->last == 10) {
+    Item* newItem = new ULListStr::Item; 
+    newItem->first = 0; 
+    newItem->last = 1;
+    newItem->prev = tail_;  
+    newItem->next = nullptr; 
+    tail_->next = newItem; 
+    tail_ = newItem; 
+    tail_->val[0] = val;  
+     
+  }
+  size_++;
+
+}
+
+void ULListStr::pop_back()
+{
+  if (size_ == 0) { 
+    return; 
+  }
+  --tail_->last; 
+  --size_; 
+
+  if (tail_->first == tail_->last) { 
+    Item* temp = tail_; 
+    tail_ = tail_->prev; 
+    if (tail_ != nullptr) { 
+      tail_->next = nullptr; 
+    }
+    else { 
+      head_ = nullptr; 
+    }
+    delete temp; 
+  }
+
+}
+
+void ULListStr::pop_front()
+{
+  if (size_ == 0) { 
+    return; 
+  }
+  ++head_->first; 
+  --size_; 
+
+  if (head_->first == head_->last) { 
+    Item* temp = head_; 
+    head_ = head_->next; 
+    if (head_ != nullptr) { 
+      head_->prev = nullptr; 
+    }
+    else {
+      tail_ = nullptr; 
+    }
+     delete temp; 
+  }
+
+}
+
+std::string const & ULListStr::back() const{
+  if (size_ == 0) {
+    return nullptr; 
+  }
+
+  return tail_->val[tail_->last-1]; 
+}
+
+  
+std::string const & ULListStr::front() const {
+  if (size_ == 0) {
+    return nullptr; 
+  }
+
+  return head_->val[head_->first]; 
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const {
+  Item* currItem = head_; 
+  
+  if (size_ == 0 || loc >= size_) { 
+    return nullptr; 
+  }
+  
+  while (currItem != nullptr) {
+    size_t count = currItem->last - currItem->first; 
+
+    if (loc < count) { 
+      return &currItem->val[currItem->first + loc]; 
+    }
+
+    loc -= count; 
+    currItem = currItem->next; 
+  }
+
+  return nullptr; 
+}
+
+
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
